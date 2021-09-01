bfcb1808e8b384a813ff1377eeb392246a38f8fd
Lindelani Mabena <lindelaniinnocent9@gmail.com>
2021-09-01 12:57:43 +0200

    updating the server

--- server.js
+++ server.js
@@ -1,27 +1,43 @@
 require('dotenv').config();
 var express = require('express');
-const helmet = require('helmet');
-const cors = require('cors')
-
+var helmet = require('helmet');
+var cors = require('cors')
+var path = require('path');
+var logger = require('morgan');
 var app = express();
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var productsRouter = require('./routes/products');
+var contactsRouter = require('./routes/contact');
+
 
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use('/users', usersRouter);
+app.use('/products', productsRouter);
+app.use('/contacts', contactsRouter);
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 
 app.get('/',(req,res)=>{
     res.send('Hello World')
 });
 
-app.get('/api',(req,res)=>
+app.get('/api/.',(req,res)=>
 {
 
 });
 
-app.get('/*',(req,res)=>{
+/* app.get('/*',(req,res)=>{
     res.send('Hello from somewhere else')
-});
+}); */
 
 app.listen(process.env.HOST_PORT, ()=>
 {
