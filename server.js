df5da490a83b7b96358a209c430e5dab7a460035
Lindelani Mabena <lindelaniinnocent9@gmail.com>
2021-09-04 15:28:41 +0200

    updated successfully

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.get('/api/.',(req,res)=>
 }); 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER_NAME}.d236u.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
-/* mongoose.connect(uri,
+ mongoose.connect(uri,
     {
         useNewUrlParser: true,
         useUnifiedTopology: true,
@@ -51,7 +51,7 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${p
         }
         console.log('Successfully connected to MongoDB');
     }
-); */
+); 
 
 
 
